5b14aefca6e3f652933b3e63e7b4732914e54f89
Steven <z5258929>
2021-10-22 18:15:41 +1100

    Wrote a test for boulder creation and movement

---
+++ src/test/java/StaticEntityTest/BoulderTest.java
@@ -0,0 +1,50 @@
+package StaticEntityTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.Boulder;
+import dungeonmania.DungeonManiaController;
+import dungeonmania.response.*;
+import dungeonmania.response.models.DungeonResponse;
+import main.java.dungeonmania;
+
+
+public class BoulderTest {
+    
+
+    @Test
+    public void createBoulderTest() {
+        DungeonManiaController game = new DungeonManiaController();
+        game.newGame("dungeonName", "Standard");
+
+        Boulder boulder = new Boulder("boulder1", "boulder", position, true);
+        
+        assertListAreEqualIgnoringOrder(Arrays.asList("boulder1"), DungeonResponse.getEntities("boulder1"));
+
+
+    }
+
+
+    @Test 
+    public void moveDownBoulderTest() {
+
+        DungeonManiaController game = new DungeonManiaController();
+        game.newGame("dungeonName", "Standard");
+        Character character = new Character("character1", "character", position(3,2,1) ,false, 100, 10, null);
+        Boulder boulder = new Boulder("boulder1", "boulder", position(3,3,1), true);
+
+        character.moveDown();
+        //game.interact("boulder1");
+        //player position
+        assertEquals(position(3,3,1), character.getPosition());
+        // boulder position
+        assertEquals(position(3,4,1), boulder.getPosition());
+
+    }
+
+
+}
