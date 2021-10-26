59e671a8f466531c9ce82d0f52cb2581f8ad36d3
Steven <z5258929>
2021-10-27 00:19:08 +1100

    added method to for checking if floor switch is triggered

--- src/main/java/dungeonmania/FloorSwitch.java
+++ src/main/java/dungeonmania/FloorSwitch.java
@@ -1,11 +1,30 @@
 package dungeonmania;
 
 import dungeonmania.util.Position;
+import dungeonmania.Boulder;
 
 public class FloorSwitch extends Entity {
 
+    public boolean isTriggered = false;
+
     public FloorSwitch(String id, String type, Position position, boolean isInteractable) {
         super(id, type, position, isInteractable);
+    }
+
+
+    
+	public boolean getisTriggered() {
+		return isTriggered;
+	}
+
+    public void setisTriggered(boolean isTriggered) {
+        this.isTriggered = isTriggered;
+    }
+ 
+    public void checkFloorSwitch(Boulder b) {
+        if (b.getPosition().equals(getPosition())){ 
+            setisTriggered(true);
+        }
     }
     
 }
