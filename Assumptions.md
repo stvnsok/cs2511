faa91a7840080ee5f8c124e407e26e4ec1cfc9e2
Bill <z5305298@ad.unsw.edu.au>
2021-10-29 12:40:18 +1100

    done some coding of newGame

---
+++ Assumptions.md
@@ -0,0 +1,2 @@
+The id of dungeons is defined "dungeon" concatenated with current dungeon number.
+The id of entities is defined as entities type concatenated with current entities number.
--- src/test/java/dungeonmania/MiscTest.java
+++ src/test/java/dungeonmania/MiscTest.java
@@ -1,6 +1,8 @@
 package dungeonmania;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,5 +11,10 @@ public class MiscTest {
     public void testDungeons() {
         assertTrue(DungeonManiaController.dungeons().size() > 0);
         assertTrue(DungeonManiaController.dungeons().contains("maze"));
+    }
+    @Test
+    public void testabc() {
+        assertDoesNotThrow(() -> DungeonManiaController.abc("advanced", "Peaceful"));
+        assertEquals(DungeonManiaController.dungeons(), "abc");
     }
 }
