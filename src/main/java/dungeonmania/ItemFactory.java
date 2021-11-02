6fb63b70016d6faf3a6a59bf8e48918d4ddc93b0
William <z5310085@ad.unsw.edu.au>
2021-11-02 18:13:42 +1100

    Item factory updated to account for key, though need to clarify keys are saved

--- src/main/java/dungeonmania/ItemFactory.java
+++ src/main/java/dungeonmania/ItemFactory.java
@@ -7,14 +7,35 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class ItemFactory {
+    private static final int sword_durability = 7;
+
     public static List<Items> createInventory (JSONArray jItems) {
         List<Items> inventory = new ArrayList<>();
         for (int i = 0; i < jItems.length(); i++) {
             JSONObject jitem = jItems.getJSONObject(i);
-            inventory.add(createItem(jitem.getString("id"), jitem.getString("type"), jitem.getInt("durability")));
+            String id = jitem.getString("id");
+            String type = jitem.getString("type");
+            int durability = jitem.getInt("durability");
+            Items item = null;
+            if (jitem.getString("type") == "key") {
+                item = createItem(id, type, durability, jitem.getString("key"));
+            } else {
+                item = createItem(id, type, durability);
+            }
+            inventory.add(item);
         }
         
         return inventory;
+    }
+
+    public static Items createItem(String id, String type) {
+        switch (type) {
+            case "sword":
+                return createItem(id, type, sword_durability);
+        
+            default:
+                return createItem(id, type, 1);
+        }
     }
 
     public static Items createItem(String id, String type, int durability) {
