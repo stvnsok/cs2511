a492cfaec4789a27fc11e3df409717b3831ed38a
William <z5310085@ad.unsw.edu.au>
2021-11-02 22:25:33 +1100

    Goals are now able to check if they are fulfilled, works with front end, testing changed

--- src/test/java/dungeonmania/GoalTests.java
+++ src/test/java/dungeonmania/GoalTests.java
@@ -10,96 +10,180 @@ import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+/**
+ * Note all tests test goal factory in the process
+ */
 public class GoalTests {
     @Test
-    public void leafGoalTest() {
-        GoalLeaf leaf = new GoalLeaf("enemies");
-        leaf.setFulfilled(true);
-        assertEquals(":enemies", leaf.getGoal());
-        assertTrue(leaf.fulfilledGoals());
+    public void exitGoalTest() {
+        List<Items> inventory = new ArrayList<>();
+        List<Entity> entities = new ArrayList<>();
+        entities.add(new Entity("2", "exit", new Position(1, 2), false));
+        Character character = new Character("1", "character", new Position(1, 1), false,
+             2, 1, inventory, entities);
+        entities.add(character);
+        JSONObject jGoal = new JSONObject();
+        jGoal.put("goal", "exit");
+        Game game = new Game("stuff", "Peaceful", entities, inventory, new ArrayList<>(), jGoal, character);
+        ExitGoalLeaf goal = new ExitGoalLeaf(game);
+        assertEquals(":exit", goal.getGoal());
+        assertFalse(goal.fulfilledGoals());
+        game.tick(null, Direction.DOWN);
+        assertTrue(goal.fulfilledGoals());
+        assertEquals("", goal.getGoal());
+    }
+
+    @Test
+    public void treasureTest() {
+        List<Items> inventory = new ArrayList<>();
+        List<Entity> entities = new ArrayList<>();
+        entities.add(new Entity("2", "treasure", new Position(1, 2), false));
+        Character character = new Character("1", "character", new Position(1, 1), false,
+             2, 1, inventory, entities);
+        entities.add(character);
+        JSONObject jGoal = new JSONObject();
+        jGoal.put("goal", "treasure");
+        Game game = new Game("stuff", "Peaceful", entities, inventory, new ArrayList<>(), jGoal, character);
+        TreasureGoalLeaf goal = new TreasureGoalLeaf(game);
+        assertFalse(goal.fulfilledGoals());
+        assertEquals(":treasure", goal.getGoal());
+        game.tick(null, Direction.DOWN);
+        assertTrue(goal.fulfilledGoals());
+        assertEquals("", goal.getGoal());
+    }
+
+    @Test
+    public void boulderTest() {
+        List<Items> inventory = new ArrayList<>();
+        List<Entity> entities = new ArrayList<>();
+        entities.add(new Entity("3", "switch", new Position(1, 3), false));
+        entities.add(new Boulder("2", "boulder", new Position(1, 2), false));
+        Character character = new Character("1", "character", new Position(1, 1), false,
+             2, 1, inventory, entities);
+        entities.add(character);
+        JSONObject jGoal = new JSONObject();
+        jGoal.put("goal", "boulders");
+        Game game = new Game("stuff", "Peaceful", entities, inventory, new ArrayList<>(), jGoal, character);
+        BoulderGoalLeaf goal = new BoulderGoalLeaf(game);
+        assertFalse(goal.fulfilledGoals());
+        assertEquals(":boulders", goal.getGoal());
+        game.tick(null, Direction.DOWN);
+        assertTrue(goal.fulfilledGoals());
+        assertEquals("", goal.getGoal());
+    }
+
+    @Test
+    public void enemyTest() {
+        List<Items> inventory = new ArrayList<>();
+        List<Entity> entities = new ArrayList<>();
+        Spider spider = new Spider("3", "spider", new Position(1, 2), false, 2, 1);
+        entities.add(spider);
+        Character character = new Character("1", "character", new Position(1, 1), false,
+             6, 4, inventory, entities);
+        entities.add(character);
+        JSONObject jGoal = new JSONObject();
+        jGoal.put("goal", "enemies");
+        Game game = new Game("stuff", "Peaceful", entities, inventory, new ArrayList<>(), jGoal, character);
+        EnemyGoalLeaf goal = new EnemyGoalLeaf(game);
+        assertFalse(goal.fulfilledGoals());
+        assertEquals(":enemies", goal.getGoal());
+        entities.remove(spider);
+        assertTrue(goal.fulfilledGoals());
+        assertEquals("", goal.getGoal());
     }
 
     @Test
     public void AndGoalTest() {
-        GoalLeaf leaf1 = new GoalLeaf("enemies");
-        leaf1.setFulfilled(true);
-        GoalLeaf leaf2 = new GoalLeaf("treasure");
-        leaf2.setFulfilled(false);
-        List<Goals> goals = new ArrayList<>(Arrays.asList(leaf1, leaf2));
-        AndGoalComposite andGoal = new AndGoalComposite(goals);
-        assertFalse(andGoal.fulfilledGoals());
-        assertEquals(":treasure", andGoal.getGoal());
-        leaf2.setFulfilled(true);
-        assertTrue(andGoal.fulfilledGoals());
-        assertEquals("", andGoal.getGoal());
-        leaf1.setFulfilled(false);
-        leaf2.setFulfilled(false);
-        assertFalse(andGoal.fulfilledGoals());
-        assertEquals("( :enemies AND :treasure )", andGoal.getGoal());
+        List<Items> inventory = new ArrayList<>();
+        List<Entity> entities = new ArrayList<>();
+        entities.add(new Entity("2", "exit", new Position(1, 3), false));
+        entities.add(new Entity("3", "treasure", new Position(1,2),false));
+        Character character = new Character("1", "character", new Position(1, 1), false,
+             2, 1, inventory, entities);
+        entities.add(character);
+        JSONObject jGoal = new JSONObject();
+        jGoal.put("goal", "AND");
+        JSONArray subgoals = new JSONArray();
+        JSONObject goal1 = new JSONObject();
+        JSONObject goal2 = new JSONObject();
+        goal1.put("goal", "exit");
+        goal2.put("goal", "treasure");
+        subgoals.put(goal1);
+        subgoals.put(goal2);
+        jGoal.put("subgoals", subgoals);
+        Game game = new Game("stuff", "Peaceful", entities, inventory, new ArrayList<>(), jGoal, character);
+        assertEquals("( :exit AND :treasure )", game.getGoals());
+        game.tick(null, Direction.DOWN);
+        assertEquals(":exit", game.getGoals());
+        game.tick(null, Direction.DOWN);
+        assertEquals("", game.getGoals());
+
     }
 
     @Test
     public void OrGoalTest() {
-        GoalLeaf leaf1 = new GoalLeaf("enemies");
-        leaf1.setFulfilled(true);
-        GoalLeaf leaf2 = new GoalLeaf("treasure");
-        leaf2.setFulfilled(false);
-        List<Goals> goals = new ArrayList<>(Arrays.asList(leaf1, leaf2));
-        OrGoalComposite OrGoal = new OrGoalComposite(goals);
-        assertTrue(OrGoal.fulfilledGoals());
-        assertEquals("", OrGoal.getGoal());
-        leaf1.setFulfilled(false);
-        assertFalse(OrGoal.fulfilledGoals());
-        assertEquals("( :enemies OR :treasure )", OrGoal.getGoal());
-    }
-
-    @Test
-    public void factoryTest() {
-        JSONObject object = new JSONObject();
-        object.put("goal", "enemies");
-        Goals leaf = GoalFactory.createGoals(object);
-        assertEquals(":enemies", leaf.getGoal());
-
-        JSONObject object2 = new JSONObject();
-        object2.put("goal", "treasure");
-
+        List<Items> inventory = new ArrayList<>();
+        List<Entity> entities = new ArrayList<>();
+        entities.add(new Entity("2", "exit", new Position(1, 3), false));
+        entities.add(new Entity("3", "treasure", new Position(1,2),false));
+        Character character = new Character("1", "character", new Position(1, 1), false,
+             2, 1, inventory, entities);
+        entities.add(character);
+        JSONObject jGoal = new JSONObject();
+        jGoal.put("goal", "OR");
         JSONArray subgoals = new JSONArray();
-        subgoals.put(object);
-        subgoals.put(object2);
-        JSONObject andGoal = new JSONObject();
-        andGoal.put("goal", "AND");
-        andGoal.put("subgoals", subgoals);
-        Goals andComp = GoalFactory.createGoals(andGoal);
-        assertEquals("( :enemies AND :treasure )", andComp.getGoal());
-
-        JSONArray subgoals2 = new JSONArray();
-        subgoals2.put(object);
-        subgoals2.put(object2);
-        JSONObject OrGoal = new JSONObject();
-        OrGoal.put("goal", "OR");
-        OrGoal.put("subgoals", subgoals2);
-        Goals orComp = GoalFactory.createGoals(OrGoal);
-        assertEquals("( :enemies OR :treasure )", orComp.getGoal());
+        JSONObject goal1 = new JSONObject();
+        JSONObject goal2 = new JSONObject();
+        goal1.put("goal", "exit");
+        goal2.put("goal", "treasure");
+        subgoals.put(goal1);
+        subgoals.put(goal2);
+        jGoal.put("subgoals", subgoals);
+        Game game = new Game("stuff", "Peaceful", entities, inventory, new ArrayList<>(), jGoal, character);
+        assertEquals("( :exit OR :treasure )", game.getGoals());
+        game.tick(null, Direction.DOWN);
+        assertEquals("", game.getGoals());
     }
+
 
     @Test
     public void complexGoal() {
-        GoalLeaf goal1 = new GoalLeaf("enemies");
-        GoalLeaf goal2 = new GoalLeaf("treasure");
-        GoalLeaf goal3 = new GoalLeaf("exit");
-        List<Goals> subGoals = new ArrayList<>();
-        subGoals.add(goal1);
-        subGoals.add(goal2);
-        OrGoalComposite orGoals = new OrGoalComposite(subGoals);
-        List<Goals> superGoals = new ArrayList<>();
-        superGoals.add(goal3);
-        superGoals.add(orGoals);
-        AndGoalComposite andGoals = new AndGoalComposite(superGoals);
-        assertEquals("( :exit AND ( :enemies OR :treasure ) )", andGoals.getGoal());
-        assertFalse(andGoals.fulfilledGoals());
-        goal1.setFulfilled(true);
-        assertFalse(andGoals.fulfilledGoals());
-        goal3.setFulfilled(true);
-        assertTrue(andGoals.fulfilledGoals());
+        List<Items> inventory = new ArrayList<>();
+        List<Entity> entities = new ArrayList<>();
+        entities.add(new Entity("2", "exit", new Position(1, 3), false));
+        entities.add(new Entity("3", "treasure", new Position(1,2),false));
+        entities.add(new Spider("4", "spider", new Position (2,4), false, 1, 1));
+        Character character = new Character("1", "character", new Position(1, 1), false,
+             2, 1, inventory, entities);
+        entities.add(character);
+        JSONObject jGoal = new JSONObject();
+        jGoal.put("goal", "AND");
+        JSONArray subgoals = new JSONArray();
+        JSONObject goal1 = new JSONObject();
+        JSONObject goal2 = new JSONObject();
+        goal1.put("goal", "exit");
+        goal2.put("goal", "OR");
+        JSONArray subgoals2 = new JSONArray();
+        JSONObject orGoal1 = new JSONObject();
+        JSONObject orGoal2 = new JSONObject();
+        orGoal1.put("goal", "treasure");
+        orGoal2.put("goal", "enemies");
+        subgoals2.put(orGoal1);
+        subgoals2.put(orGoal2);
+        goal2.put("subgoals", subgoals2);
+        subgoals.put(goal1);
+        subgoals.put(goal2);
+        jGoal.put("subgoals", subgoals);
+        Game game = new Game("stuff", "Peaceful", entities, inventory, new ArrayList<>(), jGoal, character);
+        assertEquals("( :exit AND ( :treasure OR :enemies ) )", game.getGoals());
+        game.tick(null, Direction.DOWN);
+        assertEquals(":exit", game.getGoals());
+        game.tick(null, Direction.DOWN);
+        assertEquals("", game.getGoals());
+        
+        
     }
 }
