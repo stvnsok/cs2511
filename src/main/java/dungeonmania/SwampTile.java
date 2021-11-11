0e671f3bf58703664bb7030c5137b574b51b434c
Steven <z5258929>
2021-11-11 20:42:57 +1100

    added pathfinding method, now working yet

---
+++ src/main/java/dungeonmania/SwampTile.java
@@ -0,0 +1,10 @@
+package dungeonmania;
+
+import dungeonmania.util.Position;
+
+public class SwampTile extends Entity {
+
+    public SwampTile(String id, String type, Position position, boolean isInteractable) {
+        super(id, type, position, isInteractable);
+    }
+}
