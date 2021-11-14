e8d978d0e040590c445ef596f0a3d9ec6c20beb4
EdwardLu88 <33267573+EdwardLu88@users.noreply.github.com>
2021-11-14 23:35:47 +1100

    Removed unused code

--- src/main/java/dungeonmania/FloorSwitch.java
+++ src/main/java/dungeonmania/FloorSwitch.java
@@ -2,23 +2,9 @@ package dungeonmania;
 
 import dungeonmania.util.Position;
 
-public class FloorSwitch extends Entity implements BoulderObserver {
-    private boolean isTriggered = false;
+public class FloorSwitch extends Entity {
 
     public FloorSwitch(String id, String type, Position position, boolean isInteractable) {
         super(id, type, position, isInteractable);
-    }
-
-    public boolean isTriggered() {
-        return isTriggered;
-    }
-
-    @Override
-    public void update(Position boulderOldPosition, Position boulderNewPosition) {
-        if (getPosition().equals(boulderOldPosition)) {
-            isTriggered = false;
-        } else if (getPosition().equals(boulderNewPosition)) {
-            isTriggered = true;
-        }
     }
 }
